217a84d85b4732a43bd9d8a17d4c4f69cd52b8f0
Duc Tran Van <tranvanductt@gmail.com>
2020-10-21 16:13:38 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build a deep learning architecture to predict graspabel locations for robotic manipulation.',
+    author='DucTran',
+    license='MIT',
+)
