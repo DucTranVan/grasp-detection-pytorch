b9dee968e1395da8ccf1104b9708e3cba2663f29
Duc Tran Van <tranvanductt@gmail.com>
2020-10-26 13:44:57 +0700

    done data exploratory task

---
+++ src/data/prepare_dataset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rawdatadir=../../data/raw/grasps/
+interdatadir=../../data/interim/grasp/
+mkdir -p $interdatadir
+cd $rawdatadir
+for f in *.tar.gz; do tar -xvf "$f" -C "../../interim/grasp/"; done
