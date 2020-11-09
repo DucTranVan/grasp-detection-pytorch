86a3ee9f185abfa844dd95164443f8fea39016b9
Duc Tran Van <tranvanductt@gmail.com>
2020-11-09 16:22:18 +0700

    done prepare data

---
+++ src/models/util.py
@@ -0,0 +1,11 @@
+import torch
+import PIL
+import numpy as np
+from toech.utils.data import Dataset
+
+class Cornell_Grasp_dataset(Dataset):
+    def __init__(self, path2data, transform, transform_params):
+        pass
+    def __len__(self):
+        pass
+
